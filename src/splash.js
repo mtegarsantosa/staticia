b294b82c35757a8a4bf67c2401cdd1167ae09b97
ARSAN <arsandev@users.noreply.github.com>
2020-05-04 02:46:44 +0700

    update splash

--- src/splash.js
+++ src/splash.js
@@ -1,8 +1,10 @@
 import figlet from 'figlet'
 import chalk from 'chalk'
 export default function splash(){
-  let header = figlet.textSync('static-dir', {font:"epic",horizontalLayout:"full"})+" by arsan (https://github.com/arsandev)"
-  console.log(chalk.green(header))
+  let header = figlet.textSync('Staticia', {font:"bell",horizontalLayout:"full"})
+  console.log(chalk.yellow(header))
+  console.log(chalk.yellow("by arsan (https://github.com/arsandev)"))
   console.log()
-  console.log("static-dir -help")
+  console.log("try:")
+  console.log("> staticia -help")
 }
