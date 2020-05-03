261f06095c60b34aa51b3d880da941ac77321e1f
ARSAN <arsandev@users.noreply.github.com>
2020-05-04 04:39:33 +0700

    change splash

--- src/splash.js
+++ src/splash.js
@@ -2,8 +2,8 @@ import figlet from 'figlet'
 import chalk from 'chalk'
 export default function splash(){
   let header = figlet.textSync('Staticia', {font:"bell",horizontalLayout:"full"})
+  console.log(chalk.yellow("Welcome to"))
   console.log(chalk.yellow(header))
-  console.log(chalk.yellow("by arsan (https://github.com/arsandev)"))
   console.log()
   console.log("try:")
   console.log("> staticia -help")
