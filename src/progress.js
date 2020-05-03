245a4cb4e04ede139e23e1f5d7bda991e246b019
ARSAN <arsandev@users.noreply.github.com>
2020-05-04 01:07:57 +0700

    progress strucutural

--- src/progress.js
+++ src/progress.js
@@ -1,20 +1,36 @@
 import copy from '../lib/copy'
-import generateJson from '../lib/generateJson'
-import generateIndex from '../lib/generateIndex'
+import generateData from '../lib/data'
+import generatePage from '../lib/page'
 import fs from 'fs'
+import chalk from 'chalk'
 import rimraf from 'rimraf'
+import cliProgress from 'cli-progress'
+
+const bar = new cliProgress.SingleBar({
+  format: 'Compiling... |' + chalk.yellow('{bar}') + '| {percentage}% || {value}/{total} Chunks'
+}, cliProgress.Presets.shades_classic);
 
 export default function progress(src, template="just-listed") {
   fs.mkdirSync(global.build_dir)
-  generateJson(src).then((obj) => {
-    generateIndex(obj, template)
-    copy(src)
+  bar.start(200, 0)
+  generateData(src).then(() => {
+    bar.update(50)
+    generatePage(template).then(() => {
+      bar.update(150)
+    })
+    copy(src).then(() => {
+      bar.update(200)
+      bar.stop()
+      console.log()
+      console.log()
+      console.log(chalk.yellow(`cd "${global.build_dir}"`))
+    })
   }).catch(err => {
     console.log(err)
   })
 
   process.on('uncaughtException', function(err) {
-    console.log("Progress Aborted!")
+    console.log(err)
     rimraf.sync(global.build_dir)
     process.exit(1)
   })
